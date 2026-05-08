b246f1f5726b4561266523d1551a6eeb20580241
Batubo Victory <victorybatubo76@gmail.com>
2026-05-08 12:41:27 +0100

    feat: Create mobile nav bar

--- app/client/src/layouts/Navbar.tsx
+++ app/client/src/layouts/Navbar.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 import { selectIsAuthenticated } from '@/selectors/authSelectors'
 import { useSelector } from 'react-redux'
 import ProfileMenu from '@/components/features/profile/ProfileMenu'
-
+import { MenuIcon } from 'lucide-react'
 
 const Navbar = () => {
     const isAuthenticated = useSelector(selectIsAuthenticated)
@@ -13,6 +13,8 @@ const Navbar = () => {
     // Navigation
     const navigate = useNavigate();
     const [isSticky, setIsSticky] = useState(false)
+
+    const [mobileIsOpened, setMobileIsOpened] = useState<boolean>(false)
 
 
     useEffect(() => {
@@ -30,13 +32,13 @@ const Navbar = () => {
             {/* placeholder to prevent content jump when navbar becomes fixed */}
             <div className='h-0'/>
 
-            <nav className={`h-fit py-2 px-8 flex items-center fixed justify-between z-50 left-0 right-0 transition-all duration-500 ease-in-out w-full  ${
+            <nav className={`h-fit py-2 px-8 md:flex items-center fixed justify-between z-50 left-0 right-0 transition-all duration-500 ease-in-out w-full hidden  ${
                 isSticky
                 ? 'fixed py-3 transition-all duration-200 ease-in-out bg-[rgba(133,107,71,0.8)] *:text-black backdrop-blur-lg'
                 : 'mb-20'
             }`}>
 
-                {/* Navigation Links */}
+                {/* NAVIGATION Links */}
                 <div className='flex justify-between w-1/3'>
                     <NavLink children='home' to='/' />  
 
@@ -65,6 +67,65 @@ const Navbar = () => {
                 </div>
 
             </nav>
+
+            {
+                mobileIsOpened ? (
+                    <>
+                        {/* MOBILE Nav - Opened */}
+                        <nav className={`flex flex-col h-[50vh] items-center justify-between z-100 md:hidden ${
+                            isSticky
+                            ? 'fixed py-3 transition-all duration-200 ease-in-out bg-[rgba(133,107,71,1)] w-full backdrop-blur-lg'
+                            : 'mb-20'
+                        }`}>
+                            {/* Placeholder Logo */}
+                            <NavLink to={'/'} className='w-1/3'>
+                                <h3 className='text-primary font-serif underline underline-offset-8 decoration-secondary border-t-2 border-primary border-x-2 pl-2 pr-2.5 flex-1'>
+                                    LS
+                                </h3>
+                            </NavLink>  
+
+                            {/* NAVIGATION Links */}
+                            <NavLink children='home' to='/' />  
+
+                            <NavLink children='about' to='/about' />             
+                            
+                            <NavLink children='listings' to='/listings' />
+
+                            {
+                                isAuthenticated ? (
+                                    <ProfileMenu />
+                                ) : (
+                                    <Button variant='outline' onClick={() => navigate('/login')}>
+                                        Log In
+                                    </Button>
+                                )
+                            }
+                        </nav>
+                    </>
+                ) : (
+                    <>
+                        {/* MOBILE Nav - Closed */}
+                        <nav className={`flex items-center justify-between z-100 md:hidden py-3 ${
+                            isSticky
+                            ? 'fixed py-3 transition-all duration-200 ease-in-out bg-[rgba(133,107,71,1)] w-full backdrop-blur-lg'
+                            : null
+                        }`}>
+                            {/* Placeholder Logo */}
+                            <NavLink to={'/'} className='w-1/3'>
+                                <h3 className='text-primary font-serif underline underline-offset-8 decoration-secondary border-t-2 border-primary border-x-2 pl-2 pr-2.5 flex-1'>
+                                    LS
+                                </h3>
+                            </NavLink>  
+
+                            <Button onClick={() => setMobileIsOpened(true)}>
+                                <MenuIcon size={24} color='#000'/>
+                            </Button>
+                        </nav>
+                    </>
+                )
+            }
+
+            
         </>
     )
 }
