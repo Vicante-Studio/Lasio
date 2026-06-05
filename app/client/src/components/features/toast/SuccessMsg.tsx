a5e0b1091029b3c5324440e7698e43e1191734b1
Batubo Victory <victorybatubo76@gmail.com>
2026-06-05 15:15:28 +0100

    style: Add smooth-scroll effect on Main-layout

---
+++ app/client/src/components/features/toast/SuccessMsg.tsx
@@ -0,0 +1,9 @@
+const Successmsg = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Successmsg
--- app/client/src/layouts/MainLayout.tsx
+++ app/client/src/layouts/MainLayout.tsx
@@ -1,11 +1,19 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 interface MainLayoutProps {
     children: React.ReactNode;
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0,0)
+  }, [pathname])
+  
   return (
     <main>
       <Navbar />
