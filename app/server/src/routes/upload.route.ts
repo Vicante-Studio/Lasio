267a0a4acc42eb79b419f1f8475113edd39387ae
Victory Batubo <victorybatubo76@gmail.com>
2026-07-02 10:51:33 +0000

    feat: Verify user is authenticated before uploading

--- app/server/src/routes/upload.route.ts
+++ app/server/src/routes/upload.route.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { handleUploadFiles } from '../controllers/upload.controller.js';
+import { verifyToken } from '../middleware/auth.middleware.js';
 
 const router = express.Router()
 
-router.post('/upload', handleUploadFiles)
+router.post('/upload', verifyToken, handleUploadFiles)
