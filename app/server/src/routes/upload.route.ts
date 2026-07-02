2b71e0dfc48faa5810d1917fb406eff24cba17c2
Victory Batubo <victorybatubo76@gmail.com>
2026-07-02 10:49:16 +0000

    refactor: Remove multer logic from route

--- app/server/src/routes/upload.route.ts
+++ app/server/src/routes/upload.route.ts
@@ -1,9 +1,6 @@
 import express from 'express'
-import multer from 'multer';
 import { handleUploadFiles } from '../controllers/upload.controller.js';
 
 const router = express.Router()
-
-const upload = multer({ storage: multer.memoryStorage() })
 
 router.post('/upload', handleUploadFiles)
