20141334952b7f2d54c2b1014888c50399a60bf9
Victory Batubo <victorybatubo76@gmail.com>
2026-07-02 14:47:53 +0000

    feat: Create route for handling file uploads

--- app/server/src/routes/upload.route.ts
+++ app/server/src/routes/upload.route.ts
@@ -1,7 +1,9 @@
 import express from 'express'
-import { handleUploadFiles } from '../controllers/upload.controller.js';
+import { handleUploadImageFiles } from '../controllers/upload.controller.js';
 import { verifyToken } from '../middleware/auth.middleware.js';
 
 const router = express.Router()
 
-router.post('/upload', verifyToken, handleUploadFiles)
+router.post('/image', verifyToken, handleUploadImageFiles)
+
+export default router
