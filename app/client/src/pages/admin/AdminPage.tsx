e8508029aba436dfaae4d048a6f6618c86a775aa
Victory Batubo <victorybatubo76@gmail.com>
2026-07-29 10:31:00 +0000

    feat: Map the sideBar content on the Admin page

--- app/client/src/pages/admin/AdminPage.tsx
+++ app/client/src/pages/admin/AdminPage.tsx
@@ -1,8 +1,47 @@
+import { selectCurrentUser } from '@/selectors/authSelectors'
+import { useSelector } from 'react-redux'
+import SideBar from '@/features/dashboard/Sidebar'
+import { sideBarContent } from '@/data/sideBarData'
+
 const AdminPage = () => {
+    const user = useSelector(selectCurrentUser)
+    const currentDate = new Date().toDateString()
+    const firstName = user?.full_name.split(' ')[1]
+
+    
     return(
-        <>
-        
-        </>
+        <main className='p-8 w-full h-full flex'>
+            {/* Side bar */}
+            <section className='border-r border-gray-300 h-screen flex flex-col gap-8 pr-4 max-w-[20%] '>
+                {
+                    sideBarContent.map(content => (
+                        <SideBar title={content.title} description={content.description} Icon={content.Icon}/>
+                    ))
+                }
+            </section>
+
+           <section>
+                {/* Top bar */}
+                <section className='border-b-2 border-gray-300'>
+                    <h4>Dashboard</h4>
+                </section>
+                <section className='flex justify-between'>
+                    <div>
+                        <p>Welcome back, {firstName}</p>
+                        <p>Here's a quick look at your properties and activity.</p>
+                    </div>
+                    <div>
+                        <p>{currentDate}</p>
+                        <div></div>
+                    </div>
+                </section>
+
+                {/* Main content Area */}
+                <section>
+
+                </section>
+           </section>
+        </main>
     )
 }
 
