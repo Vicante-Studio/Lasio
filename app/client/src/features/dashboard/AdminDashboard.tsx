7b6d263c1902d11fcf1f0c0729c797c21b7ff6c7
Victory Batubo <victorybatubo76@gmail.com>
2026-07-29 11:02:18 +0000

    feat: Import current user

--- app/client/src/features/dashboard/AdminDashboard.tsx
+++ app/client/src/features/dashboard/AdminDashboard.tsx
@@ -1,7 +1,11 @@
+import { selectCurrentUser } from '@/selectors/authSelectors'
+import { useSelector } from 'react-redux'
+
 const AdminDashboard = () => {
+    const user = useSelector(selectCurrentUser)
     return(
         <>
-        
+            {user}
         </>
     )
 }
