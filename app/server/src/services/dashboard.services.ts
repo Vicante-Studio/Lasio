c78b4eb61fb842e6a998f5942808cdfd37a4b904
Batubo Victory <victorybatubo76@gmail.com>
2026-07-30 15:59:30 +0100

    perf: Add defensive guard in case user is not an Admin or Agent

--- app/server/src/services/dashboard.services.ts
+++ app/server/src/services/dashboard.services.ts
@@ -2,7 +2,7 @@ import { dashboardRepository } from "../repositories/dashboard.repository.js";
 import { Listing } from "../types/listing.types.js";
 
 // Get admin/agent stats
-export const getAdminStats = async(id: string, role: string) => {
+export const getDashboardStats = async(id: string, role: 'admin' | 'agent') => {
   let listings: Listing[] = []
 
   if (role === 'admin'){
@@ -11,10 +11,12 @@ export const getAdminStats = async(id: string, role: string) => {
   } else if (role === 'agent') {
     // Get total listings for a specific agent if user is agent
     listings = await dashboardRepository.findAllAgentListings(id)
+  } else {
+    throw new Error('Invalid role')
   }
 
   const total_listings: number = listings.length  // Calculate total listings
-  const total_revenue: number = listings.reduce((acc, listing) => acc + listing.price, 0) // Calculate total revenue
+  const total_revenue: number = listings.reduce((acc, listing) => acc + Number(listing.price), 0) // Calculate total revenue
   const available_listings: number = listings.filter(listing => listing.is_available === true).length // filter the available ones and calculate them 
 
   return { total_listings, total_revenue, available_listings }
