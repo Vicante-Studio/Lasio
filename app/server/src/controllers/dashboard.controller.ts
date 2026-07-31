941b0bfd30373261f87a4cf5f688d52c6827f1c5
Batubo Victory <victorybatubo76@gmail.com>
2026-07-31 15:36:32 +0100

    feat: Create controller to handle GET listings request

--- app/server/src/controllers/dashboard.controller.ts
+++ app/server/src/controllers/dashboard.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { getDashboardStats } from '../services/dashboard.services.js'
+import { getDashboardStats, getAllListings } from '../services/dashboard.services.js'
 
 export const handleGetDashboardStats = async (req: Request, res: Response) => {
     try {
@@ -18,6 +18,27 @@ export const handleGetDashboardStats = async (req: Request, res: Response) => {
             error instanceof Error
             ? error.message
             : 'Failed to get Admin/Agent stats'
+        })
+    }
+}
+
+export const handleGetListings = async (req: Request, res: Response) => {
+    try {
+        const id = req.user.id // Admin's or Agent's Id
+        const role = req.user.role // User's role i.e either admin or agent
+
+        const data = await getAllListings(id, role as 'admin' | 'agent')
+
+        res.status(200).json({ 
+            success: true,
+            data
+         })
+    } catch (error) {
+        return res.status(500).json({
+        error:
+            error instanceof Error
+            ? error.message
+            : 'Failed to get Admin/Agent Listings'
         })
     }
 }
