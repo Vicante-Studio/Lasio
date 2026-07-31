c107f01fe30fb9d3a94a31ae38593b9e56b3c25e
Batubo Victory <victorybatubo76@gmail.com>
2026-07-31 15:35:44 +0100

    feat: Create Service to get all admin or agent listings

--- app/server/src/services/dashboard.services.ts
+++ app/server/src/services/dashboard.services.ts
@@ -1,8 +1,7 @@
 import { dashboardRepository } from "../repositories/dashboard.repository.js";
 import { Listing } from "../types/listing.types.js";
 
-// Get admin/agent stats
-export const getDashboardStats = async(id: string, role: 'admin' | 'agent') => {
+const getListingsByRole = async (role: 'admin' | 'agent', id?: string) => {
   let listings: Listing[] = []
 
   if (role === 'admin'){
@@ -10,14 +9,28 @@ export const getDashboardStats = async(id: string, role: 'admin' | 'agent') => {
     listings = await dashboardRepository.findAllListings()
   } else if (role === 'agent') {
     // Get total listings for a specific agent if user is agent
-    listings = await dashboardRepository.findAllAgentListings(id)
+    listings = await dashboardRepository.findAllAgentListings(id as string)
   } else {
     throw new Error('Invalid role')
   }
+
+  return listings
+}
+
+// Get admin/agent stats
+export const getDashboardStats = async(id: string, role: 'admin' | 'agent') => {
+  const listings: Listing[] = await getListingsByRole(role)
 
   const total_listings: number = listings.length  // Calculate total listings
   const total_revenue: number = listings.reduce((acc, listing) => acc + Number(listing.price), 0) // Calculate total revenue
   const available_listings: number = listings.filter(listing => listing.is_available === true).length // filter the available ones and calculate them 
 
   return { total_listings, total_revenue, available_listings }
+}
+
+// Get All Agent or Admin listings
+export const getAllListings = async(id: string, role: 'admin' | 'agent') => {
+  const listings: Listing[] = await getListingsByRole(role, id)
+
+  return listings
 }
