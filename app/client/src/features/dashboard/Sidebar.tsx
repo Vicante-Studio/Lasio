29cb1c033f408c9b467e76fe6cde0c5f592618e2
Batubo Victory <victorybatubo76@gmail.com>
2026-08-05 09:35:41 +0100

    stle: Change hover bg color

--- app/client/src/features/dashboard/Sidebar.tsx
+++ app/client/src/features/dashboard/Sidebar.tsx
@@ -2,7 +2,7 @@ import type { SideBarProps } from '@/types/DashboardElements'
 
 const SideBar = ({ title, description, Icon }: SideBarProps) => {
     return (
-        <div className='hover:bg-gray-200 hover:cursor-pointer p-2 transition ease-in-out duration-500'>
+        <div className='hover:bg-yellow-100 hover:cursor-pointer p-2 transition ease-in-out duration-500'>
             <div className='flex gap-2 items-center'>
                 <Icon size={16} className="text-gray-800" />
                 <h4 className='font-bold'>{title}</h4>
